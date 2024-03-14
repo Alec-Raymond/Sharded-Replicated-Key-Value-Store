96a3c14ebe0a1a7a2cfbc87b7b7b0682fff5f87f
Giridhar Vadhul <gvadhul@ucsc.edu>
2024-03-14 00:50:32 -0700

    Causal Metadata Handler changes - for merge

--- cmHandlers.go
+++ cmHandlers.go
@@ -21,7 +21,7 @@ type CMResponse struct {
 func (r *Replica) handlePutCM(c echo.Context) error {
 	request := new(CMRequest)
 
-	if err := c.Bind(request); err != nil || request != nil {
+	if err := c.Bind(request); err != nil || request == nil {
 		return c.JSON(http.StatusBadRequest, ErrResponse{Error: "must provide causal metadata"})
 	}
 
@@ -30,6 +30,7 @@ func (r *Replica) handlePutCM(c echo.Context) error {
 			ErrResponse{Error: "Causal Dependencies not satisfied; try again later"},
 		)
 	}
+
 	r.vc.Accept(&request.CausalMetadata, false, &r.vcLock)
 
 	return c.JSON(http.StatusOK, CMResponse{StatusText: "Updated vectorClock"})
